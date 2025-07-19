48bf5c6bfd87306fa38062df8c07f0ab9393d4d3
jose13162 <anselmojose297@gmail.com>
2025-07-18 22:48:04 -0300

    Update view routes

--- src/routes/views.ts
+++ src/routes/views.ts
@@ -5,7 +5,13 @@ export const viewRouter = Router();
 
 viewRouter.get("/sign-up", (req, res) => res.render("sign-up", { layout: "_layout" }));
 viewRouter.get("/sign-in", (req, res) => res.render("sign-in", { layout: "_layout" }));
-viewRouter.get("/dashboard/products", (req, res) => res.render("dashboard/products", { layout: "_layout" }));
-viewRouter.get("/dashboard/products/create", (req, res) => res.render("dashboard/products/create", { layout: "_layout" }));
-viewRouter.get("/dashboard/products/create", (req, res) => res.render("dashboard/products/create", { layout: "_layout" }));
+viewRouter.get("/dashboard/products", requireAuth({ returnTo: "/sign-in" }), (req, res) =>
+  res.render("dashboard/products", { layout: "_layout" })
+);
+viewRouter.get("/dashboard/products/create", requireAuth({ returnTo: "/sign-in" }), (req, res) =>
+  res.render("dashboard/products/create", { layout: "_layout" })
+);
+viewRouter.get("/dashboard/products/create", requireAuth({ returnTo: "/sign-in" }), (req, res) =>
+  res.render("dashboard/products/create", { layout: "_layout" })
+);
 viewRouter.get("/", requireAuth({ returnTo: "/sign-in" }), (req, res) => res.render("home", { layout: "_layout", user: req.user }));
