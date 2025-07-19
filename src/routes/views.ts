5b242da5ed8f70bf23d6d4d046ec3d2373700f1d
jose13162 <anselmojose297@gmail.com>
2025-07-18 22:46:46 -0300

    Add view routes

--- src/routes/views.ts
+++ src/routes/views.ts
@@ -5,4 +5,7 @@ export const viewRouter = Router();
 
 viewRouter.get("/sign-up", (req, res) => res.render("sign-up", { layout: "_layout" }));
 viewRouter.get("/sign-in", (req, res) => res.render("sign-in", { layout: "_layout" }));
+viewRouter.get("/dashboard/products", (req, res) => res.render("dashboard/products", { layout: "_layout" }));
+viewRouter.get("/dashboard/products/create", (req, res) => res.render("dashboard/products/create", { layout: "_layout" }));
+viewRouter.get("/dashboard/products/create", (req, res) => res.render("dashboard/products/create", { layout: "_layout" }));
 viewRouter.get("/", requireAuth({ returnTo: "/sign-in" }), (req, res) => res.render("home", { layout: "_layout", user: req.user }));
